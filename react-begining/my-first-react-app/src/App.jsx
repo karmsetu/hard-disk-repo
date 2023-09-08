cecdc7a0ef91ad2e5e986da23f326dc1cd0edb32
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-09-09 01:20:08 +0530

    setUp & component training

---
+++ react-begining/my-first-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Greeting from './greetings'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      < Greeting/>
+      {/* Greeting() */}
+    </>
+  )
+}
+
+export default App
