0c4c414e26ed2e2c88f8691e6f0537b58cd9b1ae
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-09-10 21:33:29 +0530

    mot much today

--- react-begining/my-CV/src/App.jsx
+++ react-begining/my-CV/src/App.jsx
@@ -1,11 +1,14 @@
 // import { useState } from 'react'
+import Edit from './edit'
+import Output from './output'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      < Edit/>
+      < Output/>
     </>
   )
 }
