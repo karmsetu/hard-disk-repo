d7c8ab987174796372e1023811e0fdacfeb51714
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-10-11 01:26:15 +0530

    self

--- node-practice/express/API/JWT/yt-ref/script.js
+++ node-practice/express/API/JWT/yt-ref/script.js
@@ -16,19 +16,29 @@ let postsData = [
     }
 ]
 
-app.get('/posts', (req, res)=>{
-    res.json(postsData)
+app.get('/posts',authenticateToken, (req, res)=>{
+    res.json(postsData.filter(post => post.userName === req.user.userName))
 })
 
 app.post('/login', (req, res)=>{
     // res.json(postsData)
     const userName = req.body.userName
-    const user = {name: userName}
+    const user = {userName: userName}
     jwt.sign(user, process.env.ACCESS_KEY)
     res.json({
         accessKey: process.env.ACCESS_KEY 
     })
 })
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.header['authorization'] 
+    const token = authHeader && authHeader.split(' ')[1] //if we have authHeader 
+    if (token ==null) { return res.sendStatus(401)};
+    jwt.verify(token, process.env.REFRESH_TOKEN, (Error, user)=>{
+        if(Error) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000)
