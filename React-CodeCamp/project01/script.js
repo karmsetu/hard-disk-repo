e4aa2176441292125a97aaecad65efe438194455
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-10-15 01:26:40 +0530

    local Lib

--- React-CodeCamp/project01/script.js
+++ React-CodeCamp/project01/script.js
@@ -1,10 +1,5 @@
-// import { createRoot } from 'react-dom/client';
-// import { createElement } from 'react';
-// Clear the existing HTML content
-// document.body.innerHTML = '<div id="app"></div>';
 
-// Render your React component instead
-// const hello = h1
-const list = <ul><li>some1</li><li>some2</li></ul>
-ReactDOM.render(list, document.getElementById('root'))
+import React from "react"
+import ReactDOM  from "react"
+console.log(ReactDOM)
 ReactDOM.render(<h1>hello</h1>, document.getElementById('root'))
