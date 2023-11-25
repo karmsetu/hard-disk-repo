7e5ff2aa12801601e9fc0603b2e9b52f9c015ea2
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2023-11-25 21:40:39 +0530

    declaring a promise

--- HTTP-networking/fetchAPI.js
+++ HTTP-networking/fetchAPI.js
@@ -1,5 +1,41 @@
-url = "http://lol"
-settings = ""
+url = `http://testUser:pass@website.com:8080/path?value=test#testHash`
+// settings = ""
 
-const response = await fetch(url, settings)
-const responseData = await response.json()
+// const response = await fetch(url, settings)
+// const responseData = await response.json()
+
+const urlObj = new URL(url)
+console.log(urlObj)
+/*URL {
+  href: 'http://testUser:pass@website.com:8080/path?value=test#testHash',
+  origin: 'http://website.com:8080',
+  protocol: 'http:',
+  username: 'testUser',
+  password: 'pass',
+  host: 'website.com:8080',
+  hostname: 'website.com',
+  port: '8080',
+  pathname: '/path',
+  search: '?value=test',
+  searchParams: URLSearchParams { 'value' => 'test' },       
+  hash: '#testHash'
+} */
+
+// MailTo
+console.log(`mailto:karmsetu8@gmail.com`)
+
+// Declaring a promise
+
+const getRandomBool = () => Math.random() >= 0.5;
+
+const selfMadePromise = new Promise((resolve, reject) => {
+    setTimeout(()=> {
+        getRandomBool() ? resolve("resolved") : reject("rejected")
+    }, 1000)
+})
+
+
+selfMadePromise
+    .then(mess => console.log(mess))
+    .catch(err => console.log(err))
+// console.log(getRandomBool())
--- following-trend/README.md
+++ following-trend/README.md
@@ -2,3 +2,14 @@
 
 ## JSONLint
 [link](https://jsonlint.com/)
+
+`http://testUser:pass@website.com:8080/path?value=test#testHash`
+http - prtocol
+testUser:pass - for login 
+#testHash - link to a specific section of a page
+
+## Caddy
+[caddy](https://caddyserver.com/)
+
+## promises 
+[promises](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise)
